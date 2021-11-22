8830e4ab680250e11fc83b86df378887b88d195c
Justin Asfour <justin.asfour0@gmail.com>
2021-11-22 00:27:17 -0500

    Removed logo in footer, removed husky

--- packages/oasis-uikit/src/components/Footer/Footer.tsx
+++ packages/oasis-uikit/src/components/Footer/Footer.tsx
@@ -65,9 +65,6 @@ const MenuItem: React.FC<FooterProps> = ({
               ))}
             </StyledList>
           ))}
-          <Box display={["none", null, "block"]}>
-            <LogoIcon width="160px" />
-          </Box>
         </Flex>
         <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
         <StyledToolsContainer
